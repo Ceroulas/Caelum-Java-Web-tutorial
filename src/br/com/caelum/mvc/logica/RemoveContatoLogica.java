ffbe67861b3edeb2b0261c35404efa1d487f8e6c
Jonathan <jonrmar@gmail.com>
2016-12-15 00:28:45 -0200

    capitulo 9 - tutorial

---
+++ src/br/com/caelum/mvc/logica/RemoveContatoLogica.java
@@ -0,0 +1,27 @@
+package br.com.caelum.mvc.logica;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.caelum.mvc.dao.ContatoDao;
+import br.com.caelum.mvc.modelo.Contato;
+
+public class RemoveContatoLogica implements Logica{
+
+	@Override
+	public String executa(HttpServletRequest req, HttpServletResponse response) throws Exception {
+			String parametro = req.getParameter("id");
+			Long id = Long.parseLong(parametro);
+			
+			Contato contato  = new Contato();
+			contato.setId(id);
+			
+			ContatoDao dao = new ContatoDao();
+			dao.remove(contato);
+			
+			System.out.println("Excluindo contato...");
+			
+			return "mvc?logica=ListaContatosLogic";
+	}
+
+}
---
+++ src/br/com/caelum/mvc/teste/TesteContatoDao.java
@@ -0,0 +1,34 @@
+package br.com.caelum.mvc.teste;
+
+import java.util.Calendar;
+
+import br.com.caelum.mvc.dao.ContatoDao;
+import br.com.caelum.mvc.modelo.Contato;
+
+public class TesteContatoDao {
+
+	public static void main(String[] args) {
+		ContatoDao dao = new ContatoDao();
+
+		Contato contato = new Contato();
+		contato.setNome("teste");
+		contato.setEmail("j@mail.com");
+		contato.setEndereco("Av. teste 888");
+		contato.setDataNascimento(Calendar.getInstance());
+
+		System.out.println("Adicionando contato.");
+		dao.adiciona(contato);
+		System.out.println("Adicionado com sucesso!");
+		
+		System.out.println("Alterando contato.");
+		contato.setNome("trocandoNome");
+		dao.altera(contato);
+		System.out.println("Contato alterado com sucesso!");
+		
+		System.out.println("Listando contatos");
+		dao.getLista();
+		System.out.println("Contatos listado com sucessos!");
+
+	}
+
+}
