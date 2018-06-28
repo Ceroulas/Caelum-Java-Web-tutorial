e03e33541ce5f867348a2f8eaa639637e7d30025
Jonathan <jonathan@ubots.com.br>
2018-06-28 15:37:42 -0300

    test 2

--- src/br/com/caelum/mvc/logica/Logica.java
+++ src/br/com/caelum/mvc/logica/Logica.java
@@ -3,8 +3,6 @@ package br.com.caelum.mvc.logica;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public interface Logica {
-	
+public interface Logica {	
 	String executa(HttpServletRequest req,HttpServletResponse response) throws Exception;
-
 }
